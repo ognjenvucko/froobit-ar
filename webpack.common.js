3e535c1036120e61a85d6590335e384e6b13bda1
Ognjen Vucetic <ognjenvucko@gmail.com>
2020-06-18 14:47:23 +0200

    added build

--- webpack.common.js
+++ webpack.common.js
@@ -11,7 +11,7 @@ module.exports = {
 	entry: './main.js',
 	output: {
 		filename: 'game.[hash].js',
-		path: path.resolve(__dirname, 'dist'),
+		path: path.resolve(__dirname, 'docs'),
 	},
 	module: {
 		rules: [
